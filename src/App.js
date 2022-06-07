3b3794d21f7147e3128406938254217ec2a5084d
Miller <Chase.Miller200@T-Mobile.com>
2022-06-07 11:28:18 -0700

    removed errors

--- src/App.js
+++ src/App.js
@@ -1,31 +1,32 @@
 import React, { useState, useEffect } from 'react';
 import dummyData from './dummyData.json'
-import axios from 'axios';
+// import axios from 'axios';
 import CompleteCalendar from './components/CompleteCalendar/CompleteCalendar'
 import './App.css'
-import moment from 'moment';
+// import moment from 'moment';
 
 function App({startDate}) {
-  const [data, setData] = useState();
+  // const [data, setData] = useState();
 
   const [date, setDate] = useState(new Date());
-  const [selectedDates, setSelectedDates] = useState(new Set())
+  const [selectedDates] = useState(new Set())
   const [savedUserDates, setSavedUserDates] = useState()
   const [savedTeamData, setSavedTeamData] = useState()
   const [monthCount, setMonthCount] = useState(0)
   const [currentCalanderDate, setCurrentCalanderDate] = useState()
 
   const URL = process.env.REACT_APP_API_ENDPOINT
-  const signedInUSerEmail = 'johnnybravo@t-mobile.com'
+  // const signedInUSerEmail = 'johnnybravo@t-mobile.com'
 
   // Fetch Data
   useEffect(() => {
     // console.log(new Date('2022-01-03T05:00:00.000+00:00'))
 
-    const getURL = URL+moment(currentCalanderDate).format("YYYY/MM/")
+    // const getURL = URL+moment(currentCalanderDate).format("YYYY/MM/")
     const getNamesData = () => {
+      /*
       axios.get(URL).then(response => {
-        setData(response.data);
+        // setData(response.data);
         // convert stored days array into hash set 0(1), use set.has to avoid looping.
         setSavedUserDates(new Set(response.data.user.daysInOffice))
         setSavedTeamData(response.data.teamMembers) 
@@ -33,11 +34,13 @@ function App({startDate}) {
       .catch(error => {
         console.log(error);
       })
-    };
+      */
 
-    setData(dummyData)
-    setSavedUserDates(new Set(dummyData.user.daysInOffice) )
-    setSavedTeamData(dummyData.teamMembers) 
+      setSavedUserDates(new Set(dummyData.user.daysInOffice) )
+      setSavedTeamData(dummyData.teamMembers) 
+    };
+    getNamesData()
+
   }, [URL, currentCalanderDate]);
 
 
