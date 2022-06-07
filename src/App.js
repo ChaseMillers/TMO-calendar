5df299738db3bd746cb65ea4d894273aeb2f495e
Noah Hradek <Noah.Hradek1@T-Mobile.com>
2022-06-07 12:14:09 -0600

    okta fix

--- src/App.js
+++ src/App.js
@@ -2,58 +2,42 @@ import React, { useState, useEffect } from 'react';
 import dummyData from './dummyData.json'
 import axios from 'axios';
 import CompleteCalendar from './components/CompleteCalendar/CompleteCalendar'
+import { LoginCallback, Security, withOktaAuth } from "@okta/okta-react";
 import './App.css'
+import { OktaAuth, toRelativeUrl } from '@okta/okta-auth-js';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Home from './components/Home';
+import { withRouter } from 'react-router-dom';
+import { Router } from 'react-router-dom';
 
-function App() {
-  const [data, setData] = useState();
 
-  const [date, setDate] = useState(new Date());
-  const [selectedDates, setSelectedDates] = useState(new Set())
-  const [savedUserDates, setSavedUserDates] = useState()
-  const [savedTeamData, setSavedTeamData] = useState()
-  const [fetchData, triggerFetch] = useState(false)
+const oktaAuth = new OktaAuth({
+  issuer: process.env.REACT_APP_OKTA_ISSUER,
+  clientId: process.env.REACT_APP_OKTA_CLIENT_ID,
+  redirectUri: window.location.origin + '/login/callback'
+});
 
-  const URL = process.env.REACT_APP_API_ENDPOINT
-  // Fetch Data
-  useEffect(() => {
-
-    const getNamesData = () => {
-      axios.get(URL).then(response => {
-        setData(response.data);
-        // convert stored days array into hash set 0(1), use set.has to avoid looping.
-        setSavedUserDates(new Set(response.data.user.daysInOffice))
-        setSavedTeamData(response.data.teamMembers) 
-      })
-      .catch(error => {
-        console.log(error);
-      })
-    };
-    // getNamesData();
-
-    setData(dummyData)
-    setSavedUserDates(new Set(dummyData.user.daysInOffice) )
-    setSavedTeamData(dummyData.teamMembers) 
-  }, [URL]);
-
+function App({ history }) {
+  const restoreOriginalUri = async (_oktaAuth, originalUri) => {
+    history.replace(toRelativeUrl(originalUri || '/', window.location.origin));
+  };
 
   return (
-    <div className='app'>
-      <div className='absolute-container'>
-        <h1 className='text-center'>In Office Planner</h1>
-          <CompleteCalendar 
-            setDate={setDate}
-            date={date} 
-            savedTeamData={savedTeamData}
-            selectedDates={selectedDates}
-            setSavedUserDates={setSavedUserDates}
-            savedUserDates={savedUserDates}
-            setSavedTeamData={setSavedTeamData}
-            triggerFetch={triggerFetch}
-            fetchData={fetchData}
-          />
-      </div>
-    </div>
+    <Security oktaAuth={oktaAuth} restoreOriginalUri={restoreOriginalUri}>
+      <Route path="/" exact={true} component={Home}/>
+      <Route path="/login/callback" component={LoginCallback}/>
+    </Security>
   );
 }
 
-export default App;
+const AppWithRouterAccess = withRouter(App);
+
+const RouterApp = () => {
+  return (
+    <BrowserRouter>
+      <AppWithRouterAccess />
+    </BrowserRouter>
+  );
+}
+
+export default RouterApp;
--- src/index.js
+++ src/index.js
@@ -4,10 +4,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { withRouter } from 'react-router-dom';
+import { Router } from 'react-router-dom';
+import RouterApp from './App';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterApp />
   </React.StrictMode>
 );
 
