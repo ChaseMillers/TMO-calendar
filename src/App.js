eb8ab5e612c65372161f6231b79fb45d8bce1b72
Miller <Chase.Miller200@T-Mobile.com>
2022-06-08 08:11:10 -0700

    deploy includes okta

--- src/App.js
+++ src/App.js
@@ -1,70 +1,39 @@
-import React, { useState, useEffect } from 'react';
-import dummyData from './dummyData.json'
-// import axios from 'axios';
-import CompleteCalendar from './components/CompleteCalendar/CompleteCalendar'
+import React from 'react';
+import { LoginCallback, Security, withOktaAuth } from "@okta/okta-react";
 import './App.css'
-// import moment from 'moment';
+import { OktaAuth, toRelativeUrl } from '@okta/okta-auth-js';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Home from './components/Home';
+import { withRouter } from 'react-router-dom';
 
-function App({startDate}) {
-  // const [data, setData] = useState();
 
-  const [date, setDate] = useState(new Date());
-  const [selectedDates] = useState(new Set())
-  const [savedUserDates, setSavedUserDates] = useState()
-  const [savedTeamData, setSavedTeamData] = useState()
-  const [monthCount, setMonthCount] = useState(0)
-  const [currentCalanderDate, setCurrentCalanderDate] = useState()
+const oktaAuth = new OktaAuth({
+  issuer: process.env.REACT_APP_OKTA_ISSUER,
+  clientId: process.env.REACT_APP_OKTA_CLIENT_ID,
+  redirectUri: window.location.origin + '/login/callback'
+});
 
-  const URL = process.env.REACT_APP_API_ENDPOINT
-  // const signedInUSerEmail = 'johnnybravo@t-mobile.com'
-
-  // Fetch Data
-  useEffect(() => {
-    // console.log(new Date('2022-01-03T05:00:00.000+00:00'))
-
-    // const getURL = URL+moment(currentCalanderDate).format("YYYY/MM/")
-    const getNamesData = () => {
-      /*
-      axios.get(URL).then(response => {
-        // setData(response.data);
-        // convert stored days array into hash set 0(1), use set.has to avoid looping.
-        setSavedUserDates(new Set(response.data.user.daysInOffice))
-        setSavedTeamData(response.data.teamMembers) 
-      })
-      .catch(error => {
-        console.log(error);
-      })
-      */
-
-      setSavedUserDates(new Set(dummyData.user.daysInOffice) )
-      setSavedTeamData(dummyData.teamMembers) 
-    };
-    getNamesData()
-
-  }, [URL, currentCalanderDate]);
-
+function App({ history }) {
+  const restoreOriginalUri = async (_oktaAuth, originalUri) => {
+    history.replace(toRelativeUrl(originalUri || '/', window.location.origin));
+  };
 
   return (
-    <div className='app'>
-      <div className='absolute-container'>
-        <h1 className='text-center'>In Office Planner</h1>
-          <CompleteCalendar
-            startDate={startDate}
-            setDate={setDate}
-            date={date} 
-            savedTeamData={savedTeamData}
-            selectedDates={selectedDates}
-            setSavedUserDates={setSavedUserDates}
-            savedUserDates={savedUserDates}
-            setSavedTeamData={setSavedTeamData}
-            monthCount={monthCount}
-            setMonthCount={setMonthCount}
-            setCurrentCalanderDate={setCurrentCalanderDate}
-            currentCalanderDate={currentCalanderDate}
-          />
-      </div>
-    </div>
+    <Security oktaAuth={oktaAuth} restoreOriginalUri={restoreOriginalUri}>
+      <Route path="/" exact={true} component={Home}/>
+      <Route path="/login/callback" component={LoginCallback}/>
+    </Security>
   );
 }
 
-export default App;
+const AppWithRouterAccess = withRouter(App);
+
+const RouterApp = () => {
+  return (
+    <BrowserRouter>
+      <AppWithRouterAccess />
+    </BrowserRouter>
+  );
+}
+
+export default RouterApp;
